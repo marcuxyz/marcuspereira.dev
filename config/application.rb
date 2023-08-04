9d9144b376eed0e465c9b88a216a518e5cedd3dc
Marcus Pereira <mpereirassa@gmail.com>
2023-08-04 17:56:14 -0300

    🔄 refactor(application.rb): remove unused code and update configuration settings 🔀 merge(application.rb): merge changes from deleted en.yml file into pt-BR.yml file
    📝 feat(pt-BR.yml): adiciona arquivo de tradução para o idioma português do Brasil

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative "boot"
 
 require "rails"
-# Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -12,26 +11,20 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module MarcuspereiraApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'America/Sao_Paulo'
+    config.i18n.default_locale = :'pt-BR'
 
-    # Don't generate system test files.
+    if ['development', 'test'].include? ENV['RAILS_ENV']
+      Dotenv::Railtie.load
+    end
+
     config.generators.system_tests = nil
   end
 end
